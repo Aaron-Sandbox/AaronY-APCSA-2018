aa76d68e0e94533726424da1ecbb9fabb83f7212
Aaron Y <aaronyu330@gmail.com>
2018-09-12 10:39:14 -0700

    Adding client code to repo

---
+++ AccessCalculate/src/DoMath.java
@@ -0,0 +1,13 @@
+/**
+ * Client code for the Calculate Library 
+ * @author Aaron Yu
+ * @version 9.6.2018
+ */
+
+public class DoMath {
+	
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-3));
+	}
+
+}
