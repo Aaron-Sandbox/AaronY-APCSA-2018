4f0e4edd6c2b57f8e320fa5d4f6e77008ca42251
DESKTOP-GF8RGT1\aaron <aaronyu330@gmail.com>
2018-11-21 15:54:22 -0800

    Adding checkpoint 3

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -20,17 +20,18 @@ public class FracCalc {
     	}
     	
     	sc.close();
+    	
+    	int[] fracOne = {-15, 4};
+    	int[] fracTwo = {-10, 4};
+    	//"-3_3/4 - -2_2/4"
+    	
+    	System.out.println(Arrays.toString(simplify(operate(fracOne, fracTwo, "+"))));
+    	System.out.println(Arrays.toString(simplify(operate(fracOne, fracTwo, "+"))));
+    	System.out.println(Arrays.toString(simplify(operate(fracOne, fracTwo, "+"))));
+    	System.out.println(Arrays.toString(simplify(operate(fracOne, fracTwo, "+"))));
 
     }
     
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input) { 
     	
     	//Splits the string into all elements by the space string
@@ -53,24 +54,33 @@ public class FracCalc {
 			curOperator += 2;
 		}
 		
+    	//An array of all the operands in improper fraction form
+    	//[[numerator_one, denominator_one], [numerator_two, denominator_two], [numerator_three, denominator_three]], etc.
     	int[][] improperOperand = toImproperFraction(operands);
     	for(int i = 0; i < improperOperand.length; i++) {
     		System.out.print(Arrays.toString(improperOperand[i])+ " ");
     	}
+    	
+    	//Checkpoint 3
+    	//Only returns the first two operands after performing the first operation on them
+    	int[] ans = simplify(operate(improperOperand[0], improperOperand[1], operators[0]));
 
-    	return "";
+    	return ans[0]+"_"+ans[1]+"/"+ans[2];
     }
     
-    //Converts an array of numbers (fractions, mixed fractions, whole numbers) to improper fraction form
+    /* 
+     * Converts an array of numbers (fractions, mixed fractions, whole numbers) to improper fraction form
+     * Ex. [[numerator_one, denominator_one], [numerator_two, denominator_two], [numerator_three, denominator_three]]
+     */
     public static int[][] toImproperFraction(String[] fractionArray){
     	int[][] operandImproper = new int[fractionArray.length][2];
     	int counter = 0;
     	
     	//Iterates through every operand in the fractionArray
     	for(String mixedFraction : fractionArray) { 
-    		//If there is a fraction in the operand
+    		
+    		//Checks if there is a fraction in each operand in the array 
     		if(mixedFraction.indexOf("/") != -1) {
-    			//Split by underscore
     			String[] arr = mixedFraction.split("_");	
     			
     			int wholeNum = 0;
@@ -93,7 +103,7 @@ public class FracCalc {
     			}
     			
     			//Adds arrays to an array
-    			int[] improperFraction = {wholeNum*denominator+numerator, denominator};
+    			int[] improperFraction = {wholeNum*denominator+(sign(wholeNum)*numerator), denominator};
     			operandImproper[counter] = improperFraction;
     			
     			//Iterates counter to be adding on to a new position in the array
@@ -101,8 +111,11 @@ public class FracCalc {
     			
     		//When the number is whole
     		} else if(mixedFraction.indexOf("/") == -1 || mixedFraction.indexOf("_") == -1){
-    			char[] allowedValues = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
     			
+    			//Creates an array of allowed characters for error checking
+    			char[] allowedValues = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-'};
+    			
+    			//Checks each character in the operand against the array of allowed values
     			for(int i = 0; i < mixedFraction.length(); i++) {
     				char character = mixedFraction.charAt(i);
     				if(!contains(allowedValues, character)) {
@@ -112,10 +125,13 @@ public class FracCalc {
     				
     			}
     			
+    			//Parses number (which is whole) to convert to int data type
     			int numerator = Integer.parseInt(mixedFraction);
     			
+    			//Add whole number to the array of improper fractions in the form [number, 1]
     			int[] improperFraction = {numerator, 1};
     			operandImproper[counter] = improperFraction;
+    			
     			
     			//Iterates counter to be adding on to a new position in the array
     			counter++;
@@ -131,56 +147,110 @@ public class FracCalc {
     public static int[] simplify(int[] improperFraction) {
     	int[] simplifiedFraction = new int[3];
     	
+    	//Gets the whole number by dividing the numerator by the denominator and truncating 
     	simplifiedFraction[0] = improperFraction[0]/improperFraction[1];
-    	simplifiedFraction[1] = improperFraction[0]%improperFraction[1];
-    	simplifiedFraction[2] = improperFraction[1];
     	
+    	//Stores the numerator and denominator values in ints
+    	//Stores them as absolute value forms because  -18/4 simplifies to -4_2/4, not -4_-2/4
+    	int num = Math.abs(improperFraction[0]%improperFraction[1]);
+    	int denom = Math.abs(improperFraction[1]);
+    	
+    	//Simplifies the new numerator/denominator and places them into an array with the whole number
+    	simplifiedFraction[1] = num/gcd(num, denom);
+    	simplifiedFraction[2] = denom/gcd(num, denom);
+    	
+    	//Returns an array in the form [whole_num, numerator, denominator]
     	return simplifiedFraction;
     }
+    
+    //Returns the greatest common denominator of two ints
+    public static int gcd(int numOne, int numTwo){
+    	for(int i = min(Math.abs(numOne), Math.abs(numTwo)); i > 0; i--){
+    		if(numOne%i==0 && numTwo%i==0){
+    			return i;
+    		}
+    	}
+    	return 1;
+    }
+    
+    //Returns the larger of two ints
+    public static int max(int numOne, int numTwo){
+    	if(numOne > numTwo){
+    		return numOne;
+    	}
+    	return numTwo;
+    }
+    
+    //Returns the smaller of two ints
+    public static int min(int numOne, int numTwo){
+    	if(numOne > numTwo){
+    		return numTwo;
+    	}
+    	return numOne;
+    }
+    
+    //Returns the sign of an int input as either -1 or 1
+    public static int sign(int num){
+    	if(num < 0){
+    		return -1;
+    	}
+    	return 1;
+    }
+    
     
     //Operates on two improper fractions based on operator
     public static int[] operate(int[] operand_one, int[] operand_two, String operation) {
     	
     	int fraction[] = new int[2];
     	
+    	int n1 = operand_one[0];
+    	int d1 = operand_one[1];
+    	
+    	int n2 = operand_two[0];
+    	int d2 = operand_two[1];
+    	
+    	
+    	
     	if(operation.equals("+") || operation.equals("-")) {
     	
 	    	//Makes the denominator the same by multiplying by 1/1 equivalent
-	    	if(operand_one[1] != operand_two[1]) {
+	    	if(d1 != d2) {
 	    		
-	    		operand_one[0] *= operand_two[1];
-	    		operand_one[1] *= operand_two[1];
+	    		int denominatorOne = d1;
+	    		int denominatorTwo = d2;
 	    		
-	    		operand_two[0] *= operand_one[1];
-	    		operand_two[1] *= operand_one[1];
+	    		n1 *= denominatorTwo;
+	    		d1 *= denominatorTwo;
+	    		n2 *= denominatorOne;
+	    		d2 *= denominatorOne;
+	    		
 	    		
 	    	}
 	    	
 	    	if(operation.equals("+")) {
-	    		fraction[0] = operand_one[0]+operand_two[0];
-	    		fraction[1] = operand_one[1];
+	    		fraction[0] = n1 + n2;
+	    		fraction[1] = d1;
 	    		
 	    	} else {
-	    		fraction[0] = operand_one[0]-operand_two[0];
-	    		fraction[1] = operand_one[1];
+	    		fraction[0] = n1 - n2;
+	    		fraction[1] = d1;
 	    	}
 	    	
     	} else if(operation.equals("*") || operation.equals("/")){
     		if(operation.equals("/")) {
-    			int numerator = operand_two[0];
-    			int denominator = operand_two[1];
+    			int numerator = n2;
+    			int denominator = d2;
     			
-    			operand_two[0] = denominator;
-    			operand_two[1] = numerator;
+    			n2 = denominator;
+    			d2 = numerator;
     		}
     		
-    		fraction[0] = operand_one[0]*operand_two[0];
-    		fraction[1] = operand_one[1]*operand_one[1];
+    		fraction[0] = n1*n2;
+    		fraction[1] = d1*d2;
     		
     	} else {
     		//TODO: Operator is not in the correct form
     	}
-    	
     	
     	return fraction;
     }
